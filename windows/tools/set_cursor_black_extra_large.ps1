bd4ccb29e745195cbece4f9eb24547a6fbd91f6c
Rich A Marino <rma92@users.noreply.github.com>
2023-10-20 13:51:42 -0400

    added windows cursor settings

---
+++ windows/tools/set_cursor_black_extra_large.ps1
@@ -0,0 +1,38 @@
+#Set up ability to call SystemParametersInfo
+$CSharpSig = @'
+[DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+public static extern bool SystemParametersInfo(
+                 uint uiAction,
+                 uint uiParam,
+                 uint pvParam,
+                 uint fWinIni);
+'@
+
+$CursorRefresh = Add-Type -MemberDefinition $CSharpSig -Name WinAPICall -Namespace SystemParamInfo -PassThru
+
+#Set cursors in reg
+$RegConnect = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]"CurrentUser","$env:COMPUTERNAME")
+$RegCursors = $RegConnect.OpenSubKey("Control Panel\Cursors",$true)
+$RegCursors.SetValue("","Windows Black")
+$RegCursors.SetValue("AppStarting","%SystemRoot%\cursors\wait_rl.cur")
+$RegCursors.SetValue("Arrow","%SystemRoot%\cursors\arrow_rl.cur")
+$RegCursors.SetValue("Crosshair","%SystemRoot%\cursors\cross_rl.cur")
+$RegCursors.SetValue("Hand","")
+$RegCursors.SetValue("Help","%SystemRoot%\cursors\help_rl.cur")
+$RegCursors.SetValue("IBeam","%SystemRoot%\cursors\beam_rl.cur")
+$RegCursors.SetValue("No","%SystemRoot%\cursors\no_rl.cur")
+$RegCursors.SetValue("NWPen","%SystemRoot%\cursors\pen_rl.cur")
+$RegCursors.SetValue("SizeAll","%SystemRoot%\cursors\move_rl.cur")
+$RegCursors.SetValue("SizeNESW","%SystemRoot%\cursors\size1_rl.cur")
+$RegCursors.SetValue("SizeNS","%SystemRoot%\cursors\size4_rl.cur")
+$RegCursors.SetValue("SizeNWSE","%SystemRoot%\cursors\size2_rl.cur")
+$RegCursors.SetValue("SizeWE","%SystemRoot%\cursors\size3_rl.cur")
+$RegCursors.SetValue("UpArrow","%SystemRoot%\cursors\up_rl.cur")
+$RegCursors.SetValue("Wait","%SystemRoot%\cursors\busy_rl.cur")
+$RegCursors.Close()
+$RegConnect.Close()
+
+#0x0057 = SPI_SETCURSORS
+$CursorRefresh::SystemParametersInfo(0x0057,0,$null,0)
+
+
