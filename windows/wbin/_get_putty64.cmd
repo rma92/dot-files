3f616014b5e0cc5f40c9a71fc36fdea0ac9d7735
Rich A Marino <rma92@users.noreply.github.com>
2023-10-20 13:34:32 -0400

    added _get_putty64.cmd

---
+++ windows/wbin/_get_putty64.cmd
@@ -0,0 +1,8 @@
+curl -L "https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe" --output putty.exe
+curl -L "https://the.earth.li/~sgtatham/putty/latest/w64/pscp.exe" --output pscp.exe
+
+curl -L "https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe" --output plink.exe
+curl -L "https://the.earth.li/~sgtatham/putty/latest/w64/pageant.exe" --output pageant.exe
+curl -L "https://the.earth.li/~sgtatham/putty/latest/w64/puttygen.exe" --output puttygen.exe
+curl -L "https://the.earth.li/~sgtatham/putty/latest/w64/pterm.exe" --output pterm.exe
+
