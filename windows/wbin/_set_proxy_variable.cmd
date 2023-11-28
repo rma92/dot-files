1cee235fe6dd01ba57b066ab0c081d539953c14a
Rich A Marino <rma92@users.noreply.github.com>
2023-11-28 10:50:42 -0500

    fixed quotes in _set_proxy_variable.cmd

--- windows/wbin/_set_proxy_variable.cmd
+++ windows/wbin/_set_proxy_variable.cmd
@@ -6,6 +6,6 @@ set http_proxy=proxy.example.gs:3128
 set https_proxy=proxy.example.gs:3128
 set ftp_proxy=proxy.example.gs:3128
 set all_proxy=proxy.example.gs:3128
-set CURL_PROXY_OPTS="--proxy ntlmproxy.ad.example.gs:3128 --proxy-ntlm"
+set CURL_PROXY_OPTS=--proxy ntlmproxy.ad.example.gs:3128 --proxy-ntlm
 
 REM if not defined CURL_PROXY_OPTS set CURL_PROXY_OPTS=
