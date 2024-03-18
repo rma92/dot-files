0d8f7c6a394233adf10281e4478e69d8cc229bb3
admin <admin@localhost>
2024-03-18 14:55:20 -0700

    fixed network ui script

--- windows/services/mk_disable_network_ui_svc.cmd
+++ windows/services/mk_disable_network_ui_svc.cmd
@@ -3,4 +3,5 @@ sc config Wcmsvc start= disabled
 sc config NcbServe Start= disabled
 sc config Netman Start= disabled
 sc config netprofm Start= disabled
-sc config nsi start= disabled
+REM this breaks dhcp
+REM sc config nsi start= disabled
