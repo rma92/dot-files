f55efbfdbf2633846ef5b9a790d89081c8dccfd3
admin <administrator@localhost>
2024-03-18 21:20:27 -0700

    Added default services tweak

---
+++ windows/services/mk_extreme_rm_default_service_tweaks.cmd
@@ -0,0 +1,18 @@
+#Set the tweeaks in my default image
+sc config AJRouter start= disabled
+sc config DiagTrack start= disabled
+sc config diagsvc start= disabled
+sc config DPS start= disabled
+sc config WdiServiceHost start= disabled
+sc config WdiSystemHost start= disabled
+sc config MapsBroker start= disabled
+sc config SSDPSRV start= disabled
+sc config SysMain start= disabled
+sc config upnphost start= disabled
+sc config WSearch start= disabled
+sc config wuauserv start= disabled
+sc config XboxGipSvc start= disabled
+sc config XblAuthManager start= disabled
+sc config XblGameSave start= disabled
+sc config XboxNetApiSvc start= disabled
+#sc config 
