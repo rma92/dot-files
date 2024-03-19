66122b5571d37d414d106f07c1d909d28d79065f
admin <administrator@localhost>
2024-03-18 20:58:51 -0700

    added update orchestrater to wu scripts

--- windows/services/disable_wu_services.cmd
+++ windows/services/disable_wu_services.cmd
@@ -1,3 +1,4 @@
 start cmd /c setsvc BITS 4
 start cmd /c setsvc WaaSMedicSvc 4
 start cmd /c setsvc wuauserv 4
+start cmd /c setsvc UsoSvc 4
--- windows/services/enable_wu_services.cmd
+++ windows/services/enable_wu_services.cmd
@@ -1,3 +1,4 @@
 start cmd /c setsvc BITS 3
 start cmd /c setsvc WaaSMedicSvc 3
 start cmd /c setsvc wuauserv 3
+start cmd /c setsvc UsoSvc 4
