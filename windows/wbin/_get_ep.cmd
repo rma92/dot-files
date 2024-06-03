be487752bbdf317b0fd625122d604b73413956fc
Administrator <Administrator@localhost>
2024-06-03 18:50:34 -0400

    updated git_ep to rename explorerpatcher for Windows 11.

--- windows/wbin/_get_ep.cmd
+++ windows/wbin/_get_ep.cmd
@@ -1,3 +1,3 @@
 if not defined CURL_PROXY_OPTS set CURL_PROXY_OPTS=
 
-curl %CURL_PROXY_OPTS% -L "https://github.com/valinet/ExplorerPatcher/releases/latest/download/ep_setup.exe" --output ep_setup.exe
+curl %CURL_PROXY_OPTS% -L "https://github.com/valinet/ExplorerPatcher/releases/latest/download/ep_setup.exe" --output ep_setup_a.exe
---
+++ windows/wbin/_setup_git.cmd
@@ -0,0 +1,4 @@
+if not defined CURL_PROXY_OPTS set CURL_PROXY_OPTS=
+
+curl %CURL_PROXY_OPTS% -L "https://github.com/git-for-windows/git/releases/download/v2.45.2.windows.1/Git-2.45.2-64-bit.exe" --output Git-2.45.2-64-bit.exe
+Git-2.45.2-64-bit.exe
