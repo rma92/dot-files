2f0923f3773ffdb75268f4c2777a969af39dce21
Administrator <administrator@localhost>
2024-08-11 12:35:21 -0400

    added pinginfoview to _Get_nirsoft1

--- windows/wbin/_get_nirsoft1.cmd
+++ windows/wbin/_get_nirsoft1.cmd
@@ -1,4 +1,6 @@
 if not defined CURL_PROXY_OPTS set CURL_PROXY_OPTS=
 
 curl %CURL_PROXY_OPTS% -L "https://www.nirsoft.net/utils/searchmyfiles-x64.zip" --output searchmyfiles-x64.zip
+curl %CURL_PROXY_OPTS% -L "https://www.nirsoft.net/utils/pinginfoview.zip" --output pinginfoview.zip
 powershell -command "Expand-Archive -Force searchmyfiles-x64.zip ."
+powershell -command "Expand-Archive -Force pinginfoview.zip ."
