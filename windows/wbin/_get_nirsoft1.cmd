d1445efbe479035a8c0a9d31dcd33c51cb1c9d20
Administrator <administrator@localhost>
2024-08-18 18:17:02 -0400

    _get_nirsoft1.cmd

--- windows/wbin/_get_nirsoft1.cmd
+++ windows/wbin/_get_nirsoft1.cmd
@@ -2,5 +2,7 @@ if not defined CURL_PROXY_OPTS set CURL_PROXY_OPTS=
 
 curl %CURL_PROXY_OPTS% -L "https://www.nirsoft.net/utils/searchmyfiles-x64.zip" --output searchmyfiles-x64.zip
 curl %CURL_PROXY_OPTS% -L "https://www.nirsoft.net/utils/pinginfoview.zip" --output pinginfoview.zip
+curl %CURL_PROXY_OPTS% -L "https://www.nirsoft.net/utils/resourcesextract-x64.zip" --output resourcesextract-x64.zip
 powershell -command "Expand-Archive -Force searchmyfiles-x64.zip ."
 powershell -command "Expand-Archive -Force pinginfoview.zip ."
+powershell -command "Expand-Archive -Force resourcesextract-x64.zip ."
