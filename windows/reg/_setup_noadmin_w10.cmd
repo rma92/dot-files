797485433ef881b4b4d2b6a30f3c1c9d14513231
Administrator <Administrator@localhost>
2024-09-15 13:35:34 +0000

    Added batch file to set up user registries only.

---
+++ windows/reg/_setup_noadmin_w10.cmd
@@ -0,0 +1,19 @@
+REM REG IMPORT AddTakeownToContextMenu.reg
+REG IMPORT AddTextMode92XToConsole-user.reg
+REG IMPORT AeroShake-Disable.reg
+REG IMPORT cmdhere.reg
+REG IMPORT "Disable Bing Searches.reg"
+REG IMPORT "ExplorerUserSettings.reg"
+REG IMPORT "marino-cmd-powershell-console-TextMode92Xfont.reg"
+REG IMPORT MouseHoverTime100.reg
+REG IMPORT PSHashifyFolder.reg
+REG IMPORT PuttyDefaultSettings-TextMode92X-small.reg
+REG IMPORT "Windows 11 Old Context Menu.reg"
+ECHO Press any key to restart explorer (note all explorer.exe)
+pause
+taskkill -f -im explorer.exe && start explorer
+
+ECHO if using Windows 10, press any key to disable new alt tab UI.
+pause
+REM do not use on Windows 11, there will be no Alt-Tab UI.
+REG IMPORT "DisableNewAltTab.reg"
