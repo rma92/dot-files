9bc9902ef407d0dc454c13fe4a719de4eaca0bc1
Administrator <administrator@localhost>
2024-10-11 15:46:08 +0100

    Fixed a bug in copy_vimfiles.cmd when there is a space in a path (2000/XP/2003

--- windows/copy_vimfiles.cmd
+++ windows/copy_vimfiles.cmd
@@ -1,4 +1,4 @@
-copy %CD%\_vimrc %USERPROFILE%\_vimrc
-ren %USERPROFILE%\vimfiles vimfiles_old
+copy "%CD%\_vimrc" "%USERPROFILE%\_vimrc"
+ren "%USERPROFILE%\vimfiles" vimfiles_old
 xcopy /E /I /Y ".\vimfiles" "%USERPROFILE%\vimfiles\"
 pause
