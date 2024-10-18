7ee3d0c025d1ebd4853bb1211920985ac5ff8af4
Administrator <administrator@localhost>
2024-10-18 13:12:03 +0100

    changed _install_vim_2003 to use local vim files

--- windows/wbin/_install_vim_2003.cmd
+++ windows/wbin/_install_vim_2003.cmd
@@ -10,18 +10,8 @@ expand unzip.ex_ unzip.ex
 unzip.exe -qq -o gvim_9.0.2189_x86.zip -d gvim90p
 SETX VIM %CD%\gvim90p\vim
 SETX VIMRUNTIME %CD%\gvim90p\vim\vim90
-@cmd /c js "https://raw.githubusercontent.com/rma92/dot-files/main/windows/_vimrc" -saveTo %CD%\gvim90p\vim\vimrc -ignoreCertError yes -force yes
-@cmd /c js "https://raw.githubusercontent.com/rma92/dot-files/main/windows/vimfiles/colors/bbx.vim" -saveTo %CD%\gvim90p\vim\vim90\colors\bbx.vim -ignoreCertError yes -force yes
-@cmd /c js "https://raw.githubusercontent.com/rma92/dot-files/main/windows/vimfiles/syntax/ps1.vim" -saveTo %CD%\gvim90p\vim\vim90\syntax\ps1.vim -ignoreCertError yes -force yes
-@cmd /c js "https://raw.githubusercontent.com/rma92/dot-files/main/windows/vimfiles/indent/ps1.vim" -saveTo %CD%\gvim90p\vim\vim90\indent\ps1.vim -ignoreCertError yes -force yes
-@cmd /c js "https://raw.githubusercontent.com/rma92/dot-files/main/windows/vimfiles/ftplugin/ps1.vim" -saveTo %CD%\gvim90p\vim\vim90\ftplugin\ps1.vim -ignoreCertError yes -force yes
-@cmd /c js "https://raw.githubusercontent.com/rma92/dot-files/main/windows/vimfiles/doc/ps1.txt" -saveTo %CD%\gvim90p\vim\vim90\doc\ps1.txt -ignoreCertError yes -force yes
-@cmd /c js "https://raw.githubusercontent.com/rma92/dot-files/main/windows/vimfiles/README_ps1.markdown" -saveTo %CD%\gvim90p\vim\vim90\doc\README_ps1.markdown -ignoreCertError yes -force yes
-@cmd /c js "https://raw.githubusercontent.com/rma92/dot-files/main/windows/wbin/gvim_bats/gvim.bat" -saveTo %CD%\gvim.bat -ignoreCertError yes -force yes
-@cmd /c js "https://raw.githubusercontent.com/rma92/dot-files/main/windows/wbin/gvim_bats/gvimdiff.bat" -saveTo %CD%\gvimdiff.bat -ignoreCertError yes -force yes
-@cmd /c js "https://raw.githubusercontent.com/rma92/dot-files/main/windows/wbin/gvim_bats/vim.bat" -saveTo %CD%\vim.bat -ignoreCertError yes -force yes
-@cmd /c js "https://raw.githubusercontent.com/rma92/dot-files/main/windows/wbin/gvim_bats/vimdiff.bat" -saveTo %CD%\vimdiff.bat -ignoreCertError yes -force yes
-
+copy ../_vimrc %CD%\gvim90p\vim\vimrc
+xcopy /E ..\vimfiles \gvim90p\vim\vim90\
 reg add "HKEY_CURRENT_USER\SOFTWARE\Classes\*\Shell\Vim" /ve /t REG_SZ /d "Edit with &Vim" /f
 reg add "HKEY_CURRENT_USER\SOFTWARE\Classes\*\Shell\Vim" /v "Icon" /t REG_SZ /d "%~dp0gvim90p\vim\vim90\gvim.exe" /f
 reg add "HKEY_CURRENT_USER\SOFTWARE\Classes\*\Shell\Vim\command" /ve /t REG_SZ /d "%~dp0gvim90p\vim\vim90\gvim.exe \"^%%1\"" /f
