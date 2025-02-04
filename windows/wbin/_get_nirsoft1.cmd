6726cd276c54b1e819ad15666825600c45f17994
Administrator <administrator@localhost>
2025-02-04 18:01:07 +0000

    added icon extract to nirsoft

--- windows/wbin/_get_nirsoft1.cmd
+++ windows/wbin/_get_nirsoft1.cmd
@@ -3,6 +3,9 @@ if not defined CURL_PROXY_OPTS set CURL_PROXY_OPTS=
 curl %CURL_PROXY_OPTS% -L "https://www.nirsoft.net/utils/searchmyfiles-x64.zip" --output searchmyfiles-x64.zip
 curl %CURL_PROXY_OPTS% -L "https://www.nirsoft.net/utils/pinginfoview.zip" --output pinginfoview.zip
 curl %CURL_PROXY_OPTS% -L "https://www.nirsoft.net/utils/resourcesextract-x64.zip" --output resourcesextract-x64.zip
+curl %CURL_PROXY_OPTS% -L "https://www.nirsoft.net/utils/iconsext.zip" --output iconsext.zip
+
 powershell -command "Expand-Archive -Force searchmyfiles-x64.zip ."
 powershell -command "Expand-Archive -Force pinginfoview.zip ."
 powershell -command "Expand-Archive -Force resourcesextract-x64.zip ."
+powershell -command "Expand-Archive -Force iconsext.zip ."
