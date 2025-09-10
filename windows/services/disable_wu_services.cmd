c50db65d86224bec0f1a6954ce24e49c6a3c79d5
Administrator <administrator@localhost>
2025-09-10 15:35:35 +0000

    Adjusted disable_wu_services to disable a scheduled task for the Medic service.

--- windows/services/disable_wu_services.cmd
+++ windows/services/disable_wu_services.cmd
@@ -2,3 +2,5 @@ start cmd /c setsvc BITS 4
 start cmd /c setsvc WaaSMedicSvc 4
 start cmd /c setsvc wuauserv 4
 start cmd /c setsvc UsoSvc 4
+start cmd /c runasti schtasks /Change /TN "\Microsoft\Windows\WaaSMedic\WaaSMedic PerformRemediation" /Disable
+@REM a copy of the PerformRemediation task is present in this directory.
