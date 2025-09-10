06fb32982dffe95c1cb6fafe380ad4715d8e028a
Administrator <administrator@localhost>
2025-09-10 15:37:36 +0000

    updated disable_wu_services.cmd

--- windows/services/disable_wu_services.cmd
+++ windows/services/disable_wu_services.cmd
@@ -3,4 +3,5 @@ start cmd /c setsvc WaaSMedicSvc 4
 start cmd /c setsvc wuauserv 4
 start cmd /c setsvc UsoSvc 4
 start cmd /c runasti schtasks /Change /TN "\Microsoft\Windows\WaaSMedic\WaaSMedic PerformRemediation" /Disable
+start cmd /c runasti schtasks /Change /TN "\Microsoft\Windows\WindowsUpdate\Scheduled Start" /Disable
 @REM a copy of the PerformRemediation task is present in this directory.
