9287cf215831a76a53121c4520e8c36a737d9c91
Administrator <administrator@localhost>
2026-06-01 13:40:03 -0500

    added  lynx-disable-cookie-warning.sh'

---
+++ linux/lynx-disable-cookie-warning.sh
@@ -0,0 +1,13 @@
+ cat > ~/.lynx/lynx.cfg <<'EOF'
+SET_COOKIES:TRUE
+ACCEPT_ALL_COOKIES:TRUE
+PERSISTENT_COOKIES:TRUE
+COOKIE_FILE:~/.lynx_cookies
+COOKIE_SAVE_FILE:~/.lynx_cookies
+EOF
+
+cat > ~/.lynxrc <<'EOF'
+accept_all_cookies=on
+EOF
+
+
